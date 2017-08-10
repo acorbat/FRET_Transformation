Corrected error on area calculation

image_process.py
@@ -272,7 +272,7 @@ def extract_attributes(img, mask, suffix='img'):
             this_ext[suffix+'_std'] = np.nanstd(img_crop)
             this_ext[suffix+'_p25'] = np.nanpercentile(img_crop, 25)
             this_ext[suffix+'_p75'] = np.nanpercentile(img_crop, 75)
-            this_ext[suffix+'_area'] = np.nansum(mask)
+            this_ext[suffix+'_area'] = np.nansum(mask==num)
             this_ext[suffix+'_nanpixs'] = np.nansum(np.isnan(img[mask==num]))
         df.append(this_ext)
     df = pd.DataFrame(df)