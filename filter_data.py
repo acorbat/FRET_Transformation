bd6a3b4efeb66f04871cfbb618a114b8895602ae
Bruno Moretti <morettibruno@gmail.com>
2017-08-24 18:50:33 -0300

    Separated particular case from modular functions.

--- filter_data.py
+++ filter_data.py
@@ -4,23 +4,12 @@ Created on Fri Aug  4 15:49:25 2017
 
 @author: Admin
 """
-import os
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
-os.chdir(r'D:\Agus\Imaging three sensors\FRET_Transformation')
 import Anisotropy_Functions as af
 import Transformation as tf
 import Caspase_Fit as cf
-
-#%% import data
-
-noErode_df = pd.read_pickle(r'D:\Agus\Imaging three sensors\aniso_para_agustin\20131212_pos30\filtered_newnoErode_df.pandas')
-old_noErode_df = pd.read_pickle(r'D:\Agus\Imaging three sensors\aniso_para_agustin\20131212_pos30\filtered_noErode_df.pandas')
-
-Erode_df = pd.read_pickle(r'D:\Agus\Imaging three sensors\aniso_para_agustin\20131212_pos30\filtered_newErode_5_df.pandas')
-old_Erode_df = pd.read_pickle(r'D:\Agus\Imaging three sensors\aniso_para_agustin\20131212_pos30\filtered_Erode_5_df.pandas')
 
 
 #%% Define constants to be used
@@ -28,7 +17,6 @@ old_Erode_df = pd.read_pickle(r'D:\Agus\Imaging three sensors\aniso_para_agustin
 timepoints = 10 #min
 fluorophores = ['TFP','mKate','YFP'] #[Cas3,Cas8,Cas9]
 Colors = {'TFP' : 'g', 'YFP' : 'y', 'mKate' : 'r'}
-sigmoid_parameters = ['base', 'amplitude','rate', 'x0']
 
 time_coarse = np.arange(0, 90*timepoints, timepoints)
 time_fine = np.arange(0, 90*timepoints)
@@ -49,87 +37,7 @@ def r_from_i_to_df(df):
     return df
 
 
-#%% Plot results
-
-def plot_all_curves(df):
-    """
-    Plots anisotropy from mean anisotropy and from mean intensity of all fluorophores for all objects.
-    """
-    for i in df.index:
-        if not all([all(np.isnan(df[fluo+'_r_mean'][i])) for fluo in fluorophores]):
-            for fluo in fluorophores:
-                plt.plot(time_coarse, df[fluo+'_r_mean'][i], Colors[fluo], label='mean r '+fluo)
-                plt.plot(time_coarse, df[fluo+'_r_from_i'][i], Colors[fluo]+'--', label='mean I '+fluo)
-                fig = plt.gcf()
-                fig.set_size_inches(7, 5)
-            plt.title(df['object'][i])
-            plt.legend(loc=4)
-            plt.show()
-            print(i)
-
-
-def plot_curves_and_areas(df):
-    """
-    Plots anisotropy from mean anisotropy and from mean intensity of all fluorophores for all objects,
-    as well as the area of the cell.
-    """
-    for i in df.index:
-        if not all([all(np.isnan(df[fluo+'_r_mean'][i])) for fluo in fluorophores]):
-            fig, axs = plt.subplots(2, 1, figsize=(8, 10), sharex=True)
-            for fluo in fluorophores:
-                axs[0].plot(time_coarse, df[fluo+'_r_mean'][i], Colors[fluo], label='mean r '+fluo)
-                axs[0].plot(time_coarse, df[fluo+'_r_from_i'][i], Colors[fluo]+'--', label='mean I '+fluo)
-                axs[0].legend(loc=4)
-                
-                axs[1].plot(time_coarse, df[fluo+'_par_area'][i], Colors[fluo], label='area '+fluo)
-                axs[1].legend(loc=3)
-            plt.suptitle(df['object'][i])
-            plt.show()
-            print(i)
-
-
-def plot_oldVSnew(new_df, old_df):
-    """
-    Plots anisotropy from mean anisotropy and from mean intensities of the old method from old_df,
-    superposed to anistropy from intensities of the new method. Subplot of area is added.
-    """
-    for i in old_df.index:
-        if not all([all(np.isnan(old_df[fluo+'_r_mean'][i])) for fluo in fluorophores]):
-            fig, axs = plt.subplots(2, 1, figsize=(8, 10), sharex=True)
-            for fluo in fluorophores:
-                axs[0].plot(time_coarse, old_df[fluo+'_r_mean'][i], Colors[fluo], label='old mean r '+fluo)
-                axs[0].plot(time_coarse, old_df[fluo+'_r_from_i'][i], Colors[fluo]+'--', label='old mean I '+fluo)
-                axs[0].plot(time_coarse, new_df[fluo+'_r_from_i'][i], Colors[fluo]+'-.', label='new mean I '+fluo)
-                axs[0].legend(loc=4)
-                
-                axs[1].plot(time_coarse, new_df[fluo+'_par_area'][i], Colors[fluo], label='area '+fluo)
-                axs[1].legend(loc=3)
-            plt.suptitle(new_df['object'][i])
-            plt.show()
-            print(i)
-
-
-def plot_all_curves_withFit(df):
-    """
-    Plots anisotropy from mean anisotropy and from mean intensities of the old method from old_df,
-    superposed to sigmoid fit chosen.
-    """
-    for i in df.index:
-        if any([df[fluo+'_ok_1'][i] for fluo in fluorophores]):
-            for fluo in fluorophores:
-                plt.plot(time_coarse, df[fluo+'_r_mean'][i], Colors[fluo], label='mean r '+fluo)
-                plt.plot(time_coarse, df[fluo+'_r_from_i'][i], Colors[fluo]+'--', label='mean I '+fluo)
-                popt = [df[fluo+'_base'][i], df[fluo+'_amplitude'][i], df[fluo+'_rate'][i], df[fluo+'_x0'][i]]
-                plt.plot(time_fine, cf.sigmoid(time_fine, *popt), 'x'+Colors[fluo], label=fluo+' fit')
-                fig = plt.gcf()
-                fig.set_size_inches(7, 5)
-            plt.title(df['object'][i])
-            plt.legend(loc=4)
-            plt.show()
-            print(i)
-
-
-#%% Useful Functions
+#%% question function and obvious filter
 
 def apoptotic_popts(base, amplitude, rate, x0):
     """
@@ -213,7 +121,8 @@ def first_filter(df, col_to_filter='r_from_i'):
 
 def second_filter(df, col_to_filter='r_from_i'):
     """
-    Sweeps through the accepted as apoptotic curves asking which of the plausible popts is the best.
+    Sweeps through the accepted as apoptotic curves asking which of the plausible 
+    popts is the best.
     """
     for fluo in fluorophores:
         best_popts = []
@@ -293,7 +202,7 @@ def set_popts(df):
     return df
 
 
-def add_pre_post(df, col, colname):
+def add_pre_post(df, col, colname, timepoints=10):
     """
     Uses sigmoid parameters to estimate mean pre and post values of col curve.
     colname is the suffix of the new column where results are saved.
@@ -316,122 +225,3 @@ def add_pre_post(df, col, colname):
         df[fluo+'_'+colname+'_pre'] = pres
         df[fluo+'_'+colname+'_pos'] = posts
     return df
-
-
-#%% generate pdf with scatter of pre vs pos
-
-from matplotlib.backends.backend_pdf import PdfPages
-
-pp = PdfPages(r'D:\Agus\Imaging three sensors\aniso_para_agustin\20131212_pos30\preVSpos.pdf')
-
-for fluo in fluorophores:
-    plt.scatter(old_noErode_df[fluo+'_r_mean_pos'].values, old_noErode_df[fluo+'_r_mean_pre'].values, color=Colors[fluo])
-    plt.title(fluo+' no erode old mean r')
-    plt.xlabel('pos Anisotropy')
-    plt.ylabel('pre Anisotropy')
-    pp.savefig()
-    plt.show()
-    
-    plt.scatter(old_noErode_df[fluo+'_r_from_i_pos'].values, old_noErode_df[fluo+'_r_from_i_pre'].values, color=Colors[fluo])
-    plt.title(fluo+' no erode old r from mean i')
-    plt.xlabel('pos Anisotropy')
-    plt.ylabel('pre Anisotropy')
-    pp.savefig()
-    plt.show()
-    
-    plt.scatter(old_Erode_df[fluo+'_r_mean_pos'].values, old_Erode_df[fluo+'_r_mean_pre'].values, color=Colors[fluo])
-    plt.title(fluo+' erode 5 old mean r')
-    plt.xlabel('pos Anisotropy')
-    plt.ylabel('pre Anisotropy')
-    pp.savefig()
-    plt.show()
-    
-    plt.scatter(old_Erode_df[fluo+'_r_from_i_pos'].values, old_Erode_df[fluo+'_r_from_i_pre'].values, color=Colors[fluo])
-    plt.title(fluo+' erode 5 old r from mean i')
-    plt.xlabel('pos Anisotropy')
-    plt.ylabel('pre Anisotropy')
-    pp.savefig()
-    plt.show()
-    
-    plt.scatter(noErode_df[fluo+'_r_from_i_pos'].values, noErode_df[fluo+'_r_from_i_pre'].values, color=Colors[fluo])
-    plt.title(fluo+' no erode new r from mean i')
-    plt.xlabel('pos Anisotropy')
-    plt.ylabel('pre Anisotropy')
-    pp.savefig()
-    plt.show()
-    
-    plt.scatter(Erode_df[fluo+'_r_from_i_pos'].values, Erode_df[fluo+'_r_from_i_pre'].values, color=Colors[fluo])
-    plt.title(fluo+' erode 5 new r from mean i')
-    plt.xlabel('pos Anisotropy')
-    plt.ylabel('pre Anisotropy')
-    pp.savefig()
-    plt.show()
-    
-    plt.scatter(old_noErode_df[fluo+'_r_mean_pos'].values, old_noErode_df[fluo+'_r_mean_pre'].values, color=Colors[fluo], label=fluo+' no erode old mean r')
-    plt.scatter(old_noErode_df[fluo+'_r_from_i_pos'].values, old_noErode_df[fluo+'_r_from_i_pre'].values, color=Colors[fluo], marker='x', label=fluo+' no erode old r from mean i')
-    plt.title(fluo+' no erode old mean r vs no erode old r from mean i')
-    plt.xlabel('pos Anisotropy')
-    plt.ylabel('pre Anisotropy')
-    pp.savefig()
-    plt.show()
-    
-    plt.scatter(old_Erode_df[fluo+'_r_mean_pos'].values, old_Erode_df[fluo+'_r_mean_pre'].values, color=Colors[fluo], label=fluo+' erode 5 old mean r')
-    plt.scatter(old_Erode_df[fluo+'_r_from_i_pos'].values, old_Erode_df[fluo+'_r_from_i_pre'].values, color=Colors[fluo], marker='x', label=fluo+' erode 5 old r from mean i')
-    plt.title(fluo+' erode 5 old mean r vs erode 5 old r from mean i')
-    plt.xlabel('pos Anisotropy')
-    plt.ylabel('pre Anisotropy')
-    pp.savefig()
-    plt.show()
-    
-    plt.scatter(old_noErode_df[fluo+'_r_from_i_pos'].values, old_noErode_df[fluo+'_r_from_i_pre'].values, color=Colors[fluo], label=fluo+' no erode old r from i')
-    plt.scatter(noErode_df[fluo+'_r_from_i_pos'].values, noErode_df[fluo+'_r_from_i_pre'].values, color=Colors[fluo], marker='x', label=fluo+' no erode new r from mean i')
-    plt.title(fluo+' no erode old r from i vs no erode new r from mean i')
-    plt.xlabel('pos Anisotropy')
-    plt.ylabel('pre Anisotropy')
-    pp.savefig()
-    plt.show()
-    
-    plt.scatter(old_Erode_df[fluo+'_r_from_i_pos'].values, old_Erode_df[fluo+'_r_from_i_pre'].values, color=Colors[fluo], label=fluo+' erode 5 old r from i')
-    plt.scatter(Erode_df[fluo+'_r_from_i_pos'].values, Erode_df[fluo+'_r_from_i_pre'].values, color=Colors[fluo], marker='x', label=fluo+' erode 5 new r from mean i')
-    plt.title(fluo+' erode 5 old r from i vs erode 5 new r from mean i')
-    plt.xlabel('pos Anisotropy')
-    plt.ylabel('pre Anisotropy')
-    pp.savefig()
-    plt.show()
-
-pp.close()
-
-
-#%% save plot old vs new to pdf
-
-def plot_oldVSnew_pdf(pp, new_df, old_df):
-    for i in old_df.index:
-        if not all([all(np.isnan(old_df[fluo+'_r_mean'][i])) for fluo in fluorophores]):
-            fig, axs = plt.subplots(2, 1, figsize=(8, 10), sharex=True)
-            for fluo in fluorophores:
-                axs[0].plot(time_coarse, old_df[fluo+'_r_mean'][i], Colors[fluo], label='old mean r '+fluo)
-                axs[0].plot(time_coarse, old_df[fluo+'_r_from_i'][i], Colors[fluo]+'--', label='old mean I '+fluo)
-                axs[0].plot(time_coarse, new_df[fluo+'_r_from_i'][i], Colors[fluo]+'-.', label='new mean I '+fluo)
-                axs[0].legend(loc=4)
-                axs[0].set_ylabel('Anisotropy')
-                
-                axs[1].plot(time_coarse, new_df[fluo+'_par_area'][i], Colors[fluo], label='area '+fluo)
-                axs[1].legend(loc=3)
-                axs[1].set_xlabel('time (min)')
-                axs[1].set_ylabel('Area (pxs)')
-            plt.suptitle(new_df['object'][i])
-            pp.savefig()
-            plt.show()
-            print(i)
-
-pp = PdfPages(r'D:\Agus\Imaging three sensors\aniso_para_agustin\20131212_pos30\oldVSnew_noErode.pdf')
-
-plot_oldVSnew_pdf(pp, noErode_df, old_noErode_df)
-
-pp.close()
-
-pp = PdfPages(r'D:\Agus\Imaging three sensors\aniso_para_agustin\20131212_pos30\oldVSnew_Erode.pdf')
-
-plot_oldVSnew_pdf(pp, Erode_df, old_Erode_df)
-
-pp.close()
