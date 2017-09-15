34c069b21f3788e75bdb7ad1e87ab2126c0ab968
acorbat <agustin.corbat@gmail.com>
2017-09-15 18:46:59 -0300

    Added finite difference derivative

--- examples/test_caspase_fit_derivation.py
+++ examples/test_caspase_fit_derivation.py
@@ -16,6 +16,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 from scipy.signal import savgol_filter as savgol
 from scipy.interpolate import splrep, splev
+from scipy.misc import derivative
 
 # import not registered modules
 #this_dir = pathlib.Path(r'C:\Users\Agus\Documents\Laboratorio\Imaging three sensors\Analysis')
@@ -152,3 +153,88 @@ for win, pol in itertools.product(windows, polys):
         with PdfPages(str(pdf_path)) as pp:
             df = find_complex(df, pp, window=win, poly=pol)
             df.to_pickle(str(pandas_path))
+
+
+#%% Try finite differences for derivation
+
+def find_complex(df, pp, order=5):
+    time = np.arange(0, 50*timepoints, timepoints)
+    ders = {}
+    maxs = {}
+    for fluo in fluorophores:
+        ders[fluo] = []
+        maxs[fluo] = []
+        
+    for i in df.index:
+        fig, axs = plt.subplots(2,1, sharex=True, figsize=(10,12))
+        plot = False
+        for fluo in fluorophores:
+            if np.isfinite(df[fluo+'_rate'][i]):
+                
+                r = df['r_'+fluo][i]
+                x0 = df[fluo+'_x0'][i]
+                rate = df[fluo+'_rate'][i]
+                
+                r_reg, ind = tf.sigmoid_region(x0, rate, r, minimal=0.00001)
+                r_reg = replace_nan(r_reg)
+                this_time = time[ind:ind+len(r_reg)]
+                
+                if all(np.isfinite(r_reg)) and len(r_reg)>1:
+                    plot = True
+                    
+                    def this_vect(t):
+                        ind = t//timepoints
+                        ind = np.clip(ind, 0, len(r_reg)-1)
+                        return r_reg[ind]
+                    
+                    r_der = derivative(this_vect, this_time, dx=timepoints, order=order)
+                    
+                    t = np.arange(ind*timepoints, (ind+len(r_reg))*timepoints)
+                    f = splrep(this_time, r_der, k=3, s=0)
+                    der_interp = splev(t, f, der=0)
+                    
+                    max_act = get_max_ind(der_interp)+ind*timepoints
+                    
+                    axs[0].plot(this_time, r_reg, 'o'+Colors[fluo])
+                    axs[0].set_ylabel('fraction')
+                    
+                    axs[1].plot(this_time, r_der)
+                    axs[1].plot(t, der_interp)
+                    axs[1].set_ylabel('complex')
+                    axs[1].set_xlabel('time (min.)')
+                    
+                    ders[fluo].append(r_der)
+                    maxs[fluo].append(max_act)
+                    
+                else:
+                    ders[fluo].append([np.nan])
+                    maxs[fluo].append(np.nan)
+                    
+            else:
+                ders[fluo].append([np.nan])
+                maxs[fluo].append(np.nan)
+        if plot:
+            plt.suptitle('obj:'+str(i)+' exp:'+df.Content_YFP[i]+' max:'+str(max_act))
+            last_maxs = [fluo+' '+str(maxs[fluo][-1]) for fluo in fluorophores]
+            note = '\n'.join(last_maxs)
+            pp.attach_note(note, positionRect=[100,100,100,100])
+            pp.savefig()
+        plt.close()
+        
+    for fluo in fluorophores:
+        df[fluo+'_r_complex'] = ders[fluo]
+        df[fluo+'_max_activity'] = maxs[fluo]
+        
+    return df
+
+
+for this_order in [3, 5]:
+    df = pd.read_pickle('2017-09-15_OneCaspFiltered.pandas')
+    
+    filename = 'OneCasp_derivations_order%02d.pdf' % (this_order)
+    pandasname = 'OneCasp_derivations_order%02d.pandas' % (this_order)
+    pdf_path = res_path.joinpath(filename)
+    pandas_path = res_path.joinpath(pandasname)
+    with PdfPages(str(pdf_path)) as pp:
+        df = find_complex(df, pp, order=this_order)
+        df.to_pickle(str(pandas_path))
