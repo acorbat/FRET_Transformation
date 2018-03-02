cf1606360bd43ed377f21fa16f29366d10efab44
acorbat <acorbat@df.uba.ar>
2018-03-02 14:07:25 -0300

    Added figure 3 D plot.

--- examples/2018-01-12_plot.py
+++ examples/2018-01-12_plot.py
@@ -773,7 +773,7 @@ def fig_3b(df):
 
     for fluo in fluorophores:
         plt.hist(times[fluo], color=Colors[fluo])
-        his_dir = img_dir.joinpath('fluo_onecasp_hist.png')
+        his_dir = img_dir.joinpath(fluo + '_onecasp_hist.png')
         plt.savefig(str(his_dir))
         plt.close()
 
@@ -809,6 +809,77 @@ def fig_3c(df):
     plt.tight_layout()
     plt.savefig(str(img_dir))
     plt.close()
+
+
+def fig_3d():
+    img_dir = pathlib.Path('/mnt/data/Laboratorio/Imaging three sensors/img/figure_3/')
+    img_dir = img_dir.joinpath('exp_and_sim_hist2d.png')
+
+    fluorophores = ['YFP', 'mKate', 'TFP']
+    exp_data = load_data()
+    exp_data = exp_data.query('Content == "TNF alpha"')
+    mask = [all([exp_data[fluo + '_good_der'][i] for fluo in fluorophores]) for i in exp_data.index]
+    exp_times = exp_data.TFP_to_YFP.values[mask], exp_data.TFP_to_mKate.values[mask]
+    exp_times = np.asarray(exp_times).T
+
+    sim_mod_data = load_sim(filename='earm10_varligand_4_varrecep_3_varxiap_2')
+    sim_mod_times = np.asarray((sim_mod_data.TFP_to_YFP.values, sim_mod_data.TFP_to_mKate.values)).T
+    sim_mod_times += np.random.normal(0, 2, sim_mod_times.shape)
+
+    sim_ear_data = load_sim(filename='earm10_nomodif')
+    sim_ear_times = np.asarray((sim_ear_data.TFP_to_YFP.values, sim_ear_data.TFP_to_mKate.values)).T
+    sim_ear_times += np.random.normal(0, 2, sim_ear_times.shape)
+
+    df = pd.DataFrame(exp_times, columns=['TFP_to_YFP', 'TFP_to_mKate'])
+    df['origin'] = 'exp'
+    df_sim_mod = pd.DataFrame(sim_mod_times, columns=['TFP_to_YFP', 'TFP_to_mKate'])
+    df_sim_mod['origin'] = 'sim_mod'
+    df_sim_mod = df_sim_mod.dropna(axis=0, how='any')
+
+    df_sim_ear = pd.DataFrame(sim_ear_times, columns=['TFP_to_YFP', 'TFP_to_mKate'])
+    df_sim_ear['origin'] = 'sim_ear'
+    df_sim_ear = df_sim_ear.dropna(axis=0, how='any')
+
+    df = df.append(df_sim_ear)
+    df = df.append(df_sim_mod)
+
+    cmap_dict = {'sim_mod': 'Blues',
+                 'sim_ear': 'Greens',
+                 'exp': 'Reds'}
+    color_dict = {'sim_mod': 'b',
+                  'sim_ear': 'g',
+                  'exp': 'r'}
+    g = sns.JointGrid("TFP_to_YFP", "TFP_to_mKate", df, xlim=(-40, 35), ylim=(-35, 35))
+    for origin, this_df in df.groupby("origin"):
+        sns.distplot(this_df["TFP_to_YFP"], ax=g.ax_marg_x, color=color_dict[origin])
+        sns.distplot(this_df["TFP_to_mKate"], ax=g.ax_marg_y, vertical=True, color=color_dict[origin])
+        if 'sim' in origin:
+            sns.kdeplot(this_df["TFP_to_YFP"], this_df["TFP_to_mKate"], cmap=cmap_dict[origin], alpha=0.8,
+                        ax=g.ax_joint, shade_lowest=False)
+        elif origin == 'exp':
+            plt.sca(g.ax_joint)
+            times = (this_df["TFP_to_YFP"], this_df["TFP_to_mKate"])
+            times = (times[0][np.isfinite(times[0])], times[1][np.isfinite(times[1])])
+            hist, centers_x, centers_y = np.histogram2d(times[0], times[1], range=[[-30, 35], [-30, 35]],
+                                                        bins=np.arange(-30, 36,
+                                                                       5))  # mass_histogram(times, interp=True)
+            # centers_x = (centers_x[:-1] + centers_x[1:])/2
+            # centers_y = (centers_y[:-1] + centers_y[1:])/2
+
+            hist = zoom(hist, 4, order=3)
+            max_count = np.max(hist)
+            hist = hist / max_count
+            plt.contour(hist.T, extent=(centers_x[0], centers_x[-1], centers_y[0], centers_y[-1]),
+                        levels=[0.15, 0.25, 0.5, 0.75, 0.9],
+                        cmap=cmap_dict[origin])
+
+    # plot_2dhist(sim_times)
+    # plot_show()
+    # plot_data(exp_times)
+    # plt.scatter(exp_times[:, 0], exp_times[:, 1], alpha=0.1, color='r')
+    plt.tight_layout()
+    plt.savefig(str(img_dir))
+    # plt.close()
 
 
 def fig_4b(df, ind):
